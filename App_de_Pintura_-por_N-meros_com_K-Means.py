966b2eabde0e9fc112924d8caa0b5e9db5710bd6
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2023-05-17 16:40:57 -0300

    Update App_de_Pintura_-por_N-meros_com_K-Means.py

--- App_de_Pintura_-por_N-meros_com_K-Means.py
+++ App_de_Pintura_-por_N-meros_com_K-Means.py
@@ -1,77 +1,60 @@
 import streamlit as st
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-
+import matplotlib.pyplot as plt
 
 def load_image(file):
-    img = cv2.cvtColor(np.array(file), cv2.COLOR_BGR2RGB)
+    file_bytes = np.asarray(bytearray(file.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = np.array(img, dtype=np.float64) / 255
     return img
 
-
 def apply_kmeans(img, n_clusters):
-    w, h, d = img.shape
-    img_reshaped = np.reshape(img, (w * h, d))
-    kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(img_reshaped)
-    labels = kmeans.predict(img_reshaped)
+    w, h, d = original_shape = tuple(img.shape)
+    img = np.reshape(img, (w * h, d))
+    kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(img)
+    labels = kmeans.predict(img)
     return kmeans.cluster_centers_, labels, w, h
 
-
-def reconstruct_image(cluster_centers, labels, w, h):
-    d = cluster_centers.shape[1]
-    image = np.zeros((w, h, d))
+def reconstruct_image(mean_colors, labels, w, h):
+    image = np.zeros((w, h, mean_colors.shape[1]))
     label_idx = 0
     for i in range(w):
         for j in range(h):
-            image[i][j] = cluster_centers[labels[label_idx]]
+            image[i][j] = mean_colors[labels[label_idx]]
             label_idx += 1
     return image
 
-
 def apply_canny(img):
-    img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    img_gray = (img_gray * 255).astype(np.uint8)
-    edges = cv2.Canny(img_gray, 100, 200)
-    edges = np.where(edges == 255, 0, 255)
+    img = cv2.imread("img.png", 0)
+    edges = cv2.Canny(img,100,200)
+    for i in range(edges.shape[0]):
+        for j in range(edges.shape[1]):
+            if (edges[i][j] == 0):
+                edges[i][j] = 255
+            elif (edges[i][j] == 255):
+                edges[i][j] = 0
     return edges
 
-
 def add_numbers(img, edges, cluster_centers, labels, font_size):
-    mean_sort = sorted(cluster_centers, key=lambda x: sum(x))
-    w, h, d = img.shape
-    size_x, size_y = font_size * 2, font_size * 2
-
-    for round in range(len(mean_sort)):
-        for y_ in range(0, w, size_y):
-            for x_ in range(0, h, size_x):
-                if sum(img[y_][x_]) == sum(mean_sort[round]):
-                    if np.all(edges[y_:y_ + size_y, x_:x_ + size_x] == 255):
-                        cv2.putText(
-                            img,
-                            str(round + 1),
-                            (x_, y_ + size_y // 2),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            font_size / 20,
-                            (0, 0, 0, 0),
-                            1,
-                        )
-                        break
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if edges[i][j] == 255:
+                cv2.putText(img, str(labels[i]), (j, i), cv2.FONT_HERSHEY_SIMPLEX, font_size, (0, 0, 0), 2)
     return img
 
-
 def main():
-    st.title("Paint by Numbers with K-means")
+    st.title("App de Pintura por Números com K-means")
 
-    uploaded_file = st.file_uploader("Escolha uma imagem:", type=["png", "jpg", "jpeg"])
+    uploaded_file = st.file_uploader("Escolha uma imagem...", type="jpg")
     if uploaded_file is not None:
         img = load_image(uploaded_file)
-
         st.image(img, caption="Imagem original", use_column_width=True)
 
-        n_clusters = st.slider("Quantidade de clusters:", min_value=2, max_value=50, value=10, step=1)
-        font_size = st.slider("Tamanho da fonte dos números:", min_value=5, max_value=40, value=17, step=1)
+        n_clusters = st.slider("Escolha a quantidade de clusters", min_value=1, max_value=20, value=10)
+        font_size = st.slider("Escolha o tamanho da fonte", min_value=1, max_value=5, value=2)
 
         cluster_centers, labels, w, h = apply_kmeans(img, n_clusters)
         img_kmean = reconstruct_image(cluster_centers, labels, w, h)
@@ -85,7 +68,6 @@ def main():
         img_numbered = add_numbers(img_kmean.copy(), edges, cluster_centers, labels, font_size)
 
         st.image(img_numbered, caption="Imagem final com números", use_column_width=True)
-
 
 if __name__ == "__main__":
     main()
