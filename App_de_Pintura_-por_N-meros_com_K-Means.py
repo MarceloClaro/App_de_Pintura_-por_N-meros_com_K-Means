01df52d87d15003d3dce5c75394fcdcdcb3c4e83
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2023-05-20 12:29:04 -0300

    Update App_de_Pintura_-por_N-meros_com_K-Means.py

--- App_de_Pintura_-por_N-meros_com_K-Means.py
+++ App_de_Pintura_-por_N-meros_com_K-Means.py
@@ -1,5 +1,4 @@
-
-# Importando todas as bibliotecas necessárias para o funcionamento do programa
+# Importando todas as coisas necessárias para o nosso programa funcionar.
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.utils import shuffle
@@ -9,33 +8,9 @@ from PIL import Image
 import io
 import base64
 
-# Definindo a função rgb_to_cmyk
-def rgb_to_cmyk(r, g, b):
-    if (r == 0) and (g == 0) and (b == 0):
-        return 0, 0, 0, 1
-    c = 1 - r / 255
-    m = 1 - g / 255
-    y = 1 - b / 255
 
-    min_cmy = min(c, m, y)
-    c = (c - min_cmy) / (1 - min_cmy)
-    m = (m - min_cmy) / (1 - min_cmy)
-    y = (y - min_cmy) / (1 - min_cmy)
-    k = min_cmy
-
-    return c, m, y, k
-
-# Definindo a função calculate_ml
-def calculate_ml(c, m, y, k, total_ml):
-    total_ink = c + m + y + k
-    c_ml = (c / total_ink) * total_ml
-    m_ml = (m / total_ink) * total_ml
-    y_ml = (y / total_ink) * total_ml
-    k_ml = (k / total_ink) * total_ml
-    return c_ml, m_ml, y_ml, k_ml
-
-# Definindo a classe Canvas
-class Canvas:
+# Aqui estamos criando uma nova ferramenta que chamamos de "Canvas".
+class Canvas():
     def __init__(self, src, nb_color, pixel_size=4000):
         self.src = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
         self.nb_color = nb_color
@@ -53,7 +28,8 @@ class Canvas:
         for ind, color in enumerate(colors):
             self.colormap.append([int(c * 255) for c in color])
             mask = cv2.inRange(quantified_image, color, color)
-            cnts = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[0]
+            cnts = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+            cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
             for contour in cnts:
                 _, _, width_ctr, height_ctr = cv2.boundingRect(contour)
@@ -65,9 +41,7 @@ class Canvas:
 
         return canvas, colors, quantified_image
 
-    def resize(self
-
-):
+    def resize(self):
         (height, width) = self.src.shape[:2]
         if height > width:
             dim = (int(width * self.tar_width / float(height)), self.tar_width)
@@ -96,7 +70,32 @@ class Canvas:
         out = vfunc(np.arange(width * height))
         return np.resize(out, (width, height, codebook.shape[1]))
 
-# Criando a interface do programa
+
+def rgb_to_cmyk(r, g, b):
+    if (r == 0) and (g == 0) and (b == 0):
+        return 0, 0, 0, 1
+    c = 1 - r / 255
+    m = 1 - g / 255
+    y = 1 - b / 255
+
+    min_cmy = min(c, m, y)
+    c = (c - min_cmy) / (1 - min_cmy)
+    m = (m - min_cmy) / (1 - min_cmy)
+    y = (y - min_cmy) / (1 - min_cmy)
+    k = min_cmy
+
+    return c, m, y, k
+
+
+def calculate_ml(c, m, y, k, total_ml):
+    total_ink = c + m + y + k
+    c_ml = (c / total_ink) * total_ml
+    m_ml = (m / total_ink) * total_ml
+    y_ml = (y / total_ink) * total_ml
+    k_ml = (k / total_ink) * total_ml
+    return c_ml, m_ml, y_ml, k_ml
+
+
 st.image("clube.png")
 st.title('Gerador de Paleta de Cores para Pintura por Números')
 st.subheader("Sketching and concept development")
@@ -110,23 +109,18 @@ marceloclaro@geomaker.org
 Whatsapp:(88)98158-7145 (https://www.geomaker.org/)
 """)
 
-# Carregando a imagem de referência
 uploaded_file = st.file_uploader("Escolha uma imagem", type=["jpg", "png"])
-
 st.write("""
 Apresento a vocês um aplicativo chamado "Gerador de Paleta de Cores para Pintura por Números". Esse aplicativo foi desenvolvido pelo artista plástico Marcelo Claro Laranjeira, conhecido pelo pseudônimo Marcelo Claro. Marcelo é professor de geografia na cidade de Crateús, Ceará, e também é um artista plástico autodidata.
 Este aplicativo é uma ferramenta útil para artistas plásticos, pois oferece recursos para gerar paletas de cores, criar pinturas por números, desenvolver esboços e conceitos, e explorar diferentes combinações de cores.
 Como funciona? Primeiro, você pode fazer o upload de uma imagem de referência, que pode ser uma foto, ilustração ou qualquer imagem que você deseje usar como base. Em seguida, o aplicativo utiliza o algoritmo K-means para quantificar as cores presentes na imagem. Você pode controlar o número de cores desejado através de um controle deslizante, permitindo extrair a quantidade adequada de cores para sua pintura.
-Uma vez gerada a paleta de cores, o aplicativo exibe a imagem resultante, onde cada região da imagem original é substituída pela cor correspondente da paleta. Isso permite que você visualize como sua pintura ficaria usando essas cores específicas
-
-. Além disso, o aplicativo também exibe a imagem segmentada, onde cada região da imagem original é preenchida com uma cor sólida correspondente à cor dominante da região. Isso ajuda na identificação de áreas de destaque e contrastes na imagem, facilitando o processo de esboço e desenvolvimento de conceitos.
+Uma vez gerada a paleta de cores, o aplicativo exibe a imagem resultante, onde cada região da imagem original é substituída pela cor correspondente da paleta. Isso permite que você visualize como sua pintura ficaria usando essas cores específicas. Além disso, o aplicativo também exibe a imagem segmentada, onde cada região da imagem original é preenchida com uma cor sólida correspondente à cor dominante da região. Isso ajuda na identificação de áreas de destaque e contrastes na imagem, facilitando o processo de esboço e desenvolvimento de conceitos.
 Uma característica interessante do aplicativo é a possibilidade de definir o total em mililitros de tinta antes de gerar a paleta de cores. Isso permite que você obtenha doses precisas de cada cor primária para alcançar tons exatos em suas paletas.
 No processo criativo de Marcelo Claro, ele utiliza o aplicativo como uma ferramenta complementar para sua análise da paisagem humana. Ele reúne imagens, fotos e referências como inspiração e, em seguida, faz esboços e desenvolve conceitos usando a técnica de "Sketching and concept development". Ele explora diferentes ideias, experimenta composições e cores, e utiliza as paletas de cores geradas pelo aplicativo para criar suas pinturas finais.
 O trabalho de Marcelo Claro tem como conceito central "Retratando a paisagem humana: a intersecção entre a arte e a geografia". Ele busca retratar a beleza nas coisas simples e cotidianas, explorando como a paisagem humana afeta nossa vida e como nós a modificamos. Sua abordagem geográfica e estética se complementam, permitindo uma análise mais profunda da paisagem e sua relação com nossa existência.
 Em resumo, o aplicativo "Gerador de Paleta de Cores para Pintura por Números" é uma ferramenta valiosa para artistas plásticos, oferecendo recursos para criar paletas de cores, desenvolver conceitos e explorar diferentes combinações de cores. Ele auxilia no processo criativo, permitindo visualizar e experimentar as cores antes mesmo de começar a pintar. É uma ferramenta inovadora que combina arte, tecnologia e geografia, permitindo uma análise mais profunda da paisagem humana e sua relação com nossa existência.
 """)
 
-# Verificando se uma imagem foi carregada
 if uploaded_file is not None:
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     image = cv2.imdecode(file_bytes, 1)
@@ -134,28 +128,12 @@ if uploaded_file is not None:
     st.image(image, caption='Imagem Carregada', use_column_width=True)
 
     nb_color = st.slider('Escolha o número de cores para pintar', min_value=1, max_value=80, value=2, step=1)
-
     total_ml = st.slider('Escolha o total em ml da tinta de cada cor', min_value=1, max_value=1000, value=10, step=1)
-
     pixel_size = st.slider('Escolha o tamanho do pixel da pintura', min_value=500, max_value=8000, value=4000, step=100)
 
     if st.button('Gerar'):
-
-        # Tentativa de leitura dos metadados de resolução (DPI)
-        pil_image = Image.open(io.BytesIO(file_bytes))
-        if 'dpi' in pil_image.info:
-            dpi = pil_image.info['dpi']
-            st.write(f'Resolução da imagem: {dpi} DPI')
-
-            # Calcula a dimensão física de um pixel
-
-
-            cm_per_inch = pixel_size
-            cm_per_pixel = cm_per_inch / dpi[0]  # Supõe-se que a resolução seja a mesma em ambas as direções
-            st.write(f'Tamanho do pixel: {cm_per_pixel:.4f} centímetros')
-
-        canvas = Canvas(image, nb_color, pixel_size)
         try:
+            canvas = Canvas(image, nb_color, pixel_size)
             result, colors, segmented_image = canvas.generate()
 
             # Converter imagem segmentada para np.uint8
@@ -177,33 +155,7 @@ if uploaded_file is not None:
                 total_area = segmented_image.shape[0] * segmented_image.shape[1]
                 color_percentage = (color_area / total_area) * 100
 
-                st.subheader(f"Cor {i + 1}")
-                st.write(f"Porcentagem: {color_percentage:.2f}%")
-
-                # Calcular as proporções das cores CMYK
-                r, g, b = color
-                c, m, y, k = rgb_to_cmyk(r, g, b)
-                c_ml, m_ml, y_ml, k_ml = calculate_ml(c, m, y, k, total_ml)
-
-                st.write(f"Proporções das cores CMYK:")
-                st.write(f"Ciano (Azul) (C): {c_ml:.2f} ml")
-                st.write(f"Magenta (Vermelho) (M): {m_ml:.2f} ml")
-                st.write(f"Amarelo (Y): {y_ml:.2f} ml")
-                st.write(f"Preto (K): {k_ml:.2f} ml")
-
-            result_bytes = cv2.imencode('.jpg', result)[1].tobytes()
-            st.download_button(
-                label="Baixar imagem resultante",
-                data=result_bytes,
-                file_name='result.jpg',
-                mime='image/jpeg')
-
-            segmented_image_bytes = cv2.imencode('.jpg', segmented_image)[1].tobytes()
-            st.download_button(
-                label="Baixar imagem segmentada",
-                data=segmented_image_bytes,
-                file_name='segmented.jpg',
-                mime='image/jpeg')
+                st.write(f"Porcentagem de área ocupada pela cor {i + 1}: {color_percentage:.2f}%")
 
         except Exception as e:
             st.error(f"Ocorreu um erro ao gerar a paleta de cores: {e}")
