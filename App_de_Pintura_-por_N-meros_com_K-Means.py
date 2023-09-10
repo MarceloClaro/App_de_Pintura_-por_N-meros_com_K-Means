b93dbef9917f3f11bf9122960e0f1896a70e2d80
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2023-09-10 17:42:58 -0300

    Update App_de_Pintura_-por_N-meros_com_K-Means.py

--- App_de_Pintura_-por_N-meros_com_K-Means.py
+++ App_de_Pintura_-por_N-meros_com_K-Means.py
@@ -3,8 +3,10 @@ from sklearn.cluster import KMeans
 from sklearn.utils import shuffle
 import cv2
 import streamlit as st
+from PIL import Image
+import io
 
-# Definição das cores Junguianas
+# Dicionário de cores Junguianas
 cores_junguianas = {
     '1': {
         'cor': 'Preto',
@@ -38,20 +40,34 @@ cores_junguianas = {
         'personalidade': 'A cor cinza ardósia pode indicar uma personalidade reservada, misteriosa e com uma forte presença feminina.',
         'diagnostico': 'O uso excessivo da cor cinza ardósia pode indicar uma tendência a se esconder, reprimir emoções ou evitar o autoconhecimento na expressão feminina.'
     },
-    # Adicione mais cores Junguianas conforme necessário
 }
 
-# Função para calcular proporções de tinta
-def calcular_proporcoes_tinta(c, m, y, k, total_ml):
-    total_tinta = c + m + y + k
-    c_ml = (c / total_tinta) * total_ml
-    m_ml = (m / total_tinta) * total_ml
-    y_ml = (y / total_tinta) * total_ml
-    k_ml = (k / total_tinta) * total_ml
+# Funções auxiliares
+
+def rgb_to_cmyk(r, g, b):
+    if (r == 0) and (g == 0) and (b == 0):
+        return 0, 0, 0, 1
+    c = 1 - r / 255
+    m = 1 - g / 255
+    y = 1 - b / 255
+
+    min_cmy = min(c, m, y)
+    c = (c - min_cmy) / (1 - min_cmy)
+    m = (m - min_cmy) / (1 - min_cmy)
+    y = (y - min_cmy) / (1 - min_cmy)
+    k = min_cmy
+
+    return c, m, y, k
+
+def calculate_ml(c, m, y, k, total_ml):
+    total_ink = c + m + y + k
+    c_ml = (c / total_ink) * total_ml
+    m_ml = (m / total_ink) * total_ml
+    y_ml = (y / total_ink) * total_ml
+    k_ml = (k / total_ink) * total_ml
     return c_ml, m_ml, y_ml, k_ml
 
-# Função para encontrar a cor Junguiana mais próxima
-def encontrar_cor_proxima(rgb, cores_junguianas):
+def buscar_cor_proxima(rgb, cores_junguianas):
     distancias = []
     for cor_junguiana in cores_junguianas.values():
         cor_junguiana_rgb = cor_junguiana['rgb']
@@ -60,157 +76,151 @@ def encontrar_cor_proxima(rgb, cores_junguianas):
     cor_proxima_index = np.argmin(distancias)
     return cores_junguianas[str(cor_proxima_index + 1)]
 
-# Função para gerar a paleta de cores e análise da cor Junguiana
-def gerar_paleta_e_analise(image, nb_color, total_ml, pixel_size):
-    # Converte a imagem para o formato RGB
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    
-    class Canvas():
-        def __init__(self, src, nb_color, pixel_size=4000):
-            self.src = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
-            self.nb_color = nb_color
-            self.tar_width = pixel_size
-            self.colormap = []
+# Classe Canvas para manipulação de imagem
+class Canvas():
+    def __init__(self, src, nb_color, pixel_size=4000):
+        self.src = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
+        self.nb_color = nb_color
+        self.tar_width = pixel_size
+        self.colormap = []
 
-        def generate(self):
-            im_source = self.resize()
-            clean_img = self.cleaning(im_source)
-            width, height, depth = clean_img.shape
-            clean_img = np.array(clean_img, dtype="uint8") / 255
-            quantified_image, colors = self.quantification(clean_img)
-            canvas = np.ones(quantified_image.shape[:2], dtype="uint8") * 255
+    def generate(self):
+        im_source = self.resize()
+        clean_img = self.cleaning(im_source)
+        width, height, depth = clean_img.shape
+        clean_img = np.array(clean_img, dtype="uint8") / 255
+        quantified_image, colors = self.quantification(clean_img)
+        canvas = np.ones(quantified_image.shape[:2], dtype="uint8") * 255
 
-            for ind, color in enumerate(colors):
-                self.colormap.append([int(c * 255) for c in color])
-                mask = cv2.inRange(quantified_image, color, color)
-                cnts = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-                cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+        for ind, color in enumerate(colors):
+            self.colormap.append([int(c * 255) for c in color])
+            mask = cv2.inRange(quantified_image, color, color)
+            cnts = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+            cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
-                for contour in cnts:
-                    _, _, width_ctr, height_ctr = cv2.boundingRect(contour)
-                    if width_ctr > 10 and height_ctr > 10 and cv2.contourArea(contour, True) < -100:
-                        cv2.drawContours(canvas, [contour], -1, (0, 0, 0), 1)
-                        txt_x, txt_y = contour[0][0]
-                        cv2.putText(canvas, '{:d}'.format(ind + 1), (txt_x, txt_y + 15),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+            for contour in cnts:
+                _, _, width_ctr, height_ctr = cv2.boundingRect(contour)
+                if width_ctr > 10 and height_ctr > 10 and cv2.contourArea(contour, True) < -100:
+                    cv2.drawContours(canvas, [contour], -1, (0, 0, 0), 1)
+                    txt_x, txt_y = contour[0][0]
+                    cv2.putText(canvas, '{:d}'.format(ind + 1), (txt_x, txt_y + 15),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
 
-            return canvas, colors, quantified_image
+        return canvas, colors, quantified_image
 
-        def resize(self):
-            (height, width) = self.src.shape[:2]
-            if height > width:
-                dim = (int(width * self.tar_width / float(height)), self.tar_width)
-            else:
-                dim = (self.tar_width, int(height * self.tar_width / float(width)))
-            return cv2.resize(self.src, dim, interpolation=cv2.INTER_AREA)
+    def resize(self):
+        (height, width) = self.src.shape[:2]
+        if height > width:
+            dim = (int(width * self.tar_width / float(height)), self.tar_width)
+        else:
+            dim = (self.tar_width, int(height * self.tar_width / float(width)))
+        return cv2.resize(self.src, dim, interpolation=cv2.INTER_AREA)
 
-        def cleaning(self, picture):
-            clean_pic = cv2.fastNlMeansDenoisingColored(picture, None, 10, 10, 7, 21)
-            kernel = np.ones((5, 5), np.uint8)
-            img_erosion = cv2.erode(clean_pic, kernel, iterations=1)
-            img_dilation = cv2.dilate(img_erosion, kernel, iterations=1)
-            return img_dilation
+    def cleaning(self, picture):
+        clean_pic = cv2.fastNlMeansDenoisingColored(picture, None, 10, 10, 7, 21)
+        kernel = np.ones((5, 5), np.uint8)
+        img_erosion = cv2.erode(clean_pic, kernel, iterations=1)
+        img_dilation = cv2.dilate(img_erosion, kernel, iterations=1)
+        return img_dilation
 
-        def quantification(self, picture):
-            width, height, depth = picture.shape
-            flattened = np.reshape(picture, (width * height, depth))
-            sample = shuffle(flattened)[:1000]
-            kmeans = KMeans(n_clusters=self.nb_color).fit(sample)
-            labels = kmeans.predict(flattened)
-            new_img = self.recreate_image(kmeans.cluster_centers_, labels, width, height)
-            return new_img, kmeans.cluster_centers_
+    def quantification(self, picture):
+        width, height, depth = picture.shape
+        flattened = np.reshape(picture, (width * height, depth))
+        sample = shuffle(flattened)[:1000]
+        kmeans = KMeans(n_clusters=self.nb_color).fit(sample)
+        labels = kmeans.predict(flattened)
+        new_img = self.recreate_image(kmeans.cluster_centers_, labels, width, height)
+        return new_img, kmeans.cluster_centers_
 
-        def recreate_image(self, codebook, labels, width, height):
-            vfunc = lambda x: codebook[labels[x]]
-            out = vfunc(np.arange(width * height))
-            return np.resize(out, (width, height, codebook.shape[1]))
+    def recreate_image(self, codebook, labels, width, height):
+        vfunc = lambda x: codebook[labels[x]]
+        out = vfunc(np.arange(width * height))
+        return np.resize(out, (width, height, codebook.shape[1]))
 
-    # Cria uma instância da classe Canvas
-    canvas = Canvas(image, nb_color, pixel_size)
-    
-    # Gera a paleta de cores e imagens segmentadas
-    result, colors, segmented_image = canvas.generate()
-    
-    # Encontra a cor Junguiana mais próxima da cor dominante na paleta
-    cor_dominante = encontrar_cor_proxima(colors[0], cores_junguianas)
-    
-    # Retorna os resultados
-    return result, colors, segmented_image, cor_dominante
-
-# Configuração do Streamlit
-st.set_page_config(
-    page_title="Gerador de Paleta de Cores para Pintura por Números",
-    page_icon=":art:",
-    layout="wide"
-)
-
-st.image("clube.png", use_column_width=True)
+# Início da interface do Streamlit
+st.image("clube.png")
 st.title('Gerador de Paleta de Cores para Pintura por Números')
-st.subheader("Sketching and concept development")
 
-# Barra lateral para carregar a imagem
-st.sidebar.title("Opções")
-uploaded_file = st.sidebar.file_uploader("Escolha uma imagem", type=["jpg", "png"])
+uploaded_file = st.file_uploader("Escolha uma imagem", type=["jpg", "png"])
 
-# Área principal do aplicativo
 if uploaded_file is not None:
-    st.sidebar.info("Imagem carregada com sucesso!")
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    image = cv2.imdecode(file_bytes, 1)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    st.image(image, caption='Imagem Carregada', use_column_width=True)
 
-    nb_color = st.sidebar.slider('Escolha o número de cores para pintar', min_value=1, max_value=80, value=2, step=1)
-    total_ml = st.sidebar.slider('Escolha o total em ml da tinta de cada cor', min_value=1, max_value=1000, value=10, step=1)
-    pixel_size = st.sidebar.slider('Escolha o tamanho do pixel da pintura', min_value=500, max_value=8000, value=4000, step=100)
-    
-    if st.sidebar.button('Gerar Paleta de Cores'):
-        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-        image = cv2.imdecode(file_bytes, 1)
-        
-        result, colors, segmented_image, cor_dominante = gerar_paleta_e_analise(image, nb_color, total_ml, pixel_size)
-        
-        # Exibir a imagem original
-        st.subheader("Imagem Original")
-        st.image(image, caption='Imagem Carregada', use_column_width=True)
+    nb_color = st.slider('Escolha o número de cores para pintar', min_value=1, max_value=80, value=2, step=1)
+    total_ml = st.slider('Escolha o total em ml da tinta de cada cor', min_value=1, max_value=1000, value=10, step=1)
 
-        # Análise da Cor Dominante Junguiana
-        st.subheader("Análise da Cor Dominante Junguiana")
-        st.write(f"A cor dominante na paleta é {cor_dominante['cor']}.")
-        st.write(f"Anima/Animus: {cor_dominante['anima_animus']}")
-        st.write(f"Sombra: {cor_dominante['sombra']}")
-        st.write(f"Personalidade: {cor_dominante['personalidade']}")
-        st.write(f"Diagnóstico: {cor_dominante['diagnostico']}")
+    if st.button('Gerar'):
+        canvas = Canvas(image, nb_color)
+        result, colors, segmented_image = canvas.generate()
 
-        # Exibir a paleta de cores
-        st.subheader("Paleta de Cores")
+        segmented_image = (segmented_image * 255).astype(np.uint8)
+        segmented_image = cv2.cvtColor(segmented_image, cv2.COLOR_BGR2RGB)
+
+        st.image(result, caption='Imagem Resultante', use_column_width=True)
+        st.image(segmented_image, caption='Imagem Segmentada', use_column_width=True)
+
+        # Dicionário para armazenar a área de cada cor
+        area_dict = {}
+
         for i, color in enumerate(colors):
-            color_block = np.ones((50, 50, 3), np.uint8) * color[::-1]  # Cores em formato BGR
+            color_block = np.ones((50, 50, 3), np.uint8) * color[::-1]
             st.image(color_block, caption=f'Cor {i+1}', width=50)
 
-        # Exibir as proporções da tinta CMYK e a cor Junguiana mais próxima para cada cor na paleta
-        for i, color in enumerate(colors):
-            r, g, b = color
-            c, m, y, k = rgb_to_cmyk(r, g, b)
-            c_ml, m_ml, y_ml, k_ml = calcular_proporcoes_tinta(c, m, y, k, total_ml)
             color_area = np.count_nonzero(np.all(segmented_image == color, axis=-1))
-            total_area = segmented_image.shape[0] * segmented_image.shape[1]
-            color_percentage = (color_area / total_area) * 100
-            
-            st.subheader(f"Detalhes da Cor {i+1}")
-            st.write(f"Proporções da tinta CMYK:")
-            st.write(f"Ciano (Azul) (C): {c_ml:.2f} ml")
-            st.write(f"Magenta (Vermelho) (M): {m_ml:.2f} ml")
-            st.write(f"Amarelo (Y): {y_ml:.2f} ml")
-            st.write(f"Preto (K): {k_ml:.2f} ml")
-            cor_proxima = encontrar_cor_proxima(color, cores_junguianas)
-            st.write(f"Cor Junguiana Mais Próxima: {cor_proxima['cor']}")
-            st.write(f"Anima/Animus: {cor_proxima['anima_animus']}")
-            st.write(f"Sombra: {cor_proxima['sombra']}")
-            st.write(f"Personalidade: {cor_proxima['personalidade']}")
-            st.write(f"Diagnóstico: {cor_proxima['diagnostico']}")
+            area_dict[i] = color_area
 
-        # Exibir botões de download para a imagem resultante e imagem segmentada
-        st.subheader("Download")
-        result_bytes = cv2.imencode('.jpg', result)[1].tobytes()
-        st.download_button(
-            label="Baixar imagem resultante",
-            data=result_bytes,
-            file_name='result.jpg',
-            mime='image/jpeg')
+        # Encontrar o índice da cor dominante
+        dominant_color_index = max(area_dict, key=area_dict.get)
+
+        # Encontrar a cor junguiana mais próxima da cor dominante
+        dominant_color = colors[dominant_color_index]
+        closest_jungian_color = buscar_cor_proxima(dominant_color, cores_junguianas)
+
+        # Exibir a análise da cor junguiana dominante
+        st.write(f"A cor junguiana dominante é: {closest_jungian_color['cor']}")
+        st.write(f"Anima/Animus: {closest_jungian_color['anima_animus']}")
+        st.write(f"Sombra: {closest_jungian_color['sombra']}")
+        st.write(f"Personalidade: {closest_jungian_color['personalidade']}")
+        st.write(f"Diagnóstico: {closest_jungian_color['diagnostico']}")
+
+        # Loop para analisar e mostrar todas as cores
+        for i, color in enumerate(colors):
+            analyze_and_show_color(i, color, total_ml, segmented_image)
+
+def analyze_and_show_color(color_index, rgb_color, total_ml, segmented_image):
+    # Calcular a porcentagem de pixels da cor em relação à imagem segmentada
+    total_pixels = segmented_image.shape[0] * segmented_image.shape[1]
+    color_pixels = np.count_nonzero(np.all(segmented_image == rgb_color, axis=-1))
+    color_percentage = (color_pixels / total_pixels) * 100
+
+    # Calcular a quantidade de tinta necessária
+    c, m, y, k = rgb_to_cmyk(*rgb_color)
+    c_ml, m_ml, y_ml, k_ml = calculate_ml(c, m, y, k, total_ml)
+
+    # Exibir informações sobre a cor
+    st.write(f"Cor {color_index + 1}:")
+    st.write(f"Porcentagem da imagem: {color_percentage:.2f}%")
+    st.write(f"Quantidade de tinta necessária:")
+    st.write(f"   - Ciano: {c_ml:.2f} mL")
+    st.write(f"   - Magenta: {m_ml:.2f} mL")
+    st.write(f"   - Amarelo: {y_ml:.2f} mL")
+    st.write(f"   - Preto: {k_ml:.2f} mL")
+
+# O código para download permanece o mesmo
+result_bytes = cv2.imencode('.jpg', result)[1].tobytes()
+st.download_button(
+    label="Baixar imagem resultante",
+    data=result_bytes,
+    file_name='result.jpg',
+    mime='image/jpeg')
+
+segmented_image_rgb = cv2.cvtColor(segmented_image, cv2.COLOR_BGR2RGB)
+segmented_image_bytes = cv2.imencode('.jpg', segmented_image_rgb)[1].tobytes()
+st.download_button(
+    label="Baixar imagem segmentada",
+    data=segmented_image_bytes,
+    file_name='segmented.jpg',
+    mime='image/jpeg')
